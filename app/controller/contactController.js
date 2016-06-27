a07c1482438fc5ff469f5dcd914dea548d879fe3
Ramesh chand rajwar <ramesh.rajwar@kelltontech.com>
2016-06-27 14:39:48 +0530

    test commit

--- app/controller/contactController.js
+++ app/controller/contactController.js
@@ -10,20 +10,16 @@ var contactController = function(Contact){
 	}
 	
 	var add = function(req, res) {
-		//console.log(req.body);return false;
-		//var name = req.body.name; 
-	  //var isbn = req.body.isbn; 
-	  //var author = req.body.author; 
-	  //var pages = req.body.pages;    
-	  //var description = req.body.description;  
-	  //var added_date = req.body.added_date;
 	  var contact = req.body;
 	    
-var modelContact = new Contact(); 
+      var modelContact = new Contact(); 
 	  modelContact.name=contact.name;
 	  modelContact.email=contact.email;
 	  modelContact.phone=contact.phone;
 	 	
+	 modelContact.find(function (err, data) {
+		 
+	 });	
 	  modelContact.save(function (err, data) {
 		if(!err) {
 	          res.json(200, { message: "Contact created with name: " + contact.name });    
